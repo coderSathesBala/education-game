3a8d5c3457f7ff333b7af928396ddbb4fcb34252
coderSathesBala <sathes352@gmail.com>
2022-02-03 10:48:04 -0500

    addCarImage

--- index.js
+++ index.js
@@ -16,18 +16,27 @@ class Player {
             y: 0
         }
 
-        // this.image = {
+        const image = new Image()
+        image.src = './img/car.jpg'
 
+            this.image = image
             this.width = 100,
             this.height = 100
-        // }
     }
 
     draw() {
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        // c.fillStyle = 'red'
+        // c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        c.drawImage(this.image, this.position.x, this.position.y)
     }
 }
 
 const player = new Player()
 player.draw()
+
+function animate() {
+    requestAnimationFrame(animate)
+    player.draw()
+}
+
+animate()
